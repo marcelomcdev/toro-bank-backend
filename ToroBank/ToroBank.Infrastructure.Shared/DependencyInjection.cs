15686ce1ab530f9708a13d4f4d2fac88c034a73d
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-10 14:22:56 -0300

    adds shared infrastructure layer services

---
+++ ToroBank/ToroBank.Infrastructure.Shared/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToroBank.Application.Common.Interfaces.Services;
+using ToroBank.Infrastructure.Shared.Services;
+
+namespace ToroBank.Infrastructure.Shared
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureShared(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IDateTime, DateTimeService>();
+
+            return services;
+        }
+    }
+}
---
+++ ToroBank/ToroBank.Infrastructure.Shared/Services/DateTimeService.cs
@@ -0,0 +1,9 @@
+﻿using ToroBank.Application.Common.Interfaces.Services;
+
+namespace ToroBank.Infrastructure.Shared.Services
+{
+    public class DateTimeService : IDateTime
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
