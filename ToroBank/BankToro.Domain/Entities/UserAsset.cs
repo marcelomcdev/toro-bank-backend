8cc743ac02dec80bb850ab8c2b06b88f6657c096
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-10 09:55:49 -0300

    Add user asset entity

---
+++ ToroBank/BankToro.Domain/Entities/UserAsset.cs
@@ -0,0 +1,16 @@
+﻿using ToroBank.Domain.Common;
+
+namespace ToroBank.Domain.Entities
+{
+    public class UserAsset : BaseEntity
+    {
+        protected UserAsset() { }
+        public UserAsset(int assetId, int userId)
+        {
+            AssetId = assetId;
+            UserId = userId;
+        }
+        public int AssetId { get; set; }
+        public int UserId { get; set; }
+    }
+}
