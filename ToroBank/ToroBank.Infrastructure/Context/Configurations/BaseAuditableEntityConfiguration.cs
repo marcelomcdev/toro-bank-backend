a3b28df7d2159a3e1c36a4c2cad0f0e6a7e05784
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-10 12:33:38 -0300

    Add infrastructure context configuration

---
+++ ToroBank/ToroBank.Infrastructure/Context/Configurations/BaseAuditableEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using ToroBank.Domain.Common;
+
+namespace ToroBank.Infrastructure.Persistence.Context.Configurations
+{
+    public class BaseAuditableEntityConfiguration<T, TKey> : IEntityTypeConfiguration<T>
+    where TKey : IEquatable<TKey>
+    where T : BaseAuditableEntity<TKey>
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            // IDENTITY
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+        }
+    }
+}
---
+++ ToroBank/ToroBank.Infrastructure/Context/Configurations/UserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToroBank.Domain.Entities;
+
+namespace ToroBank.Infrastructure.Persistence.Context.Configurations
+{    
+
+    public class UserConfiguration : BaseAuditableEntityConfiguration<User, int>
+    {
+        public override void Configure(EntityTypeBuilder<User> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(t => t.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(f => f.AccountNumber).IsRequired();
+            builder.Property(f => f.CPF).HasMaxLength(20).IsRequired();
+            builder.Property(f => f.Balance).IsRequired();
+            builder.Property(f => f.Username).IsRequired();
+            builder.Property(f => f.Password).IsRequired();
+        }
+    }
+
+
+}
