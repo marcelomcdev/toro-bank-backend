051c1198ab4f52118eb8711a4d983469f365705a
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-10 09:50:44 -0300

    add user entity at domain

---
+++ ToroBank/BankToro.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using ToroBank.Domain.Common;
+
+namespace ToroBank.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        protected User() { }
+        public User(int accountNumber, string name, string cpf, decimal balance = 0M)
+        {
+            AccountNumber = accountNumber;
+            Name = name;
+            CPF = cpf;
+            Balance = balance;
+        }
+        public int AccountNumber { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
