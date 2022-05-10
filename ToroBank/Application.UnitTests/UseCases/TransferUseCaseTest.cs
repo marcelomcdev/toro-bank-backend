283c22c7d558ee43242d37b364347d5b892d1533
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-10 09:49:28 -0300

    add moq to tests

--- ToroBank/Application.UnitTests/UseCases/TransferUseCaseTest.cs
+++ ToroBank/Application.UnitTests/UseCases/TransferUseCaseTest.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace Application.UnitTests.UseCases
 {
-    internal class TransferUseCaseTest
+    public class TransferUseCaseTest
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
     }
 }
