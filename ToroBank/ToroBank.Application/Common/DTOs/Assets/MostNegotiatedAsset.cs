01d6432bfed3416b330b0e63b8bd093f876a3636
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-12 18:26:05 -0300

    Add DTOs in application to generate query

---
+++ ToroBank/ToroBank.Application/Common/DTOs/Assets/MostNegotiatedAsset.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroBank.Domain.Entities;
+
+namespace ToroBank.Application.Common.DTOs.Assets
+{
+    public class MostNegotiatedAssetItem
+    {
+        public Asset Asset { get; set; }
+        public int Quantity { get; set; }
+    }
+}
