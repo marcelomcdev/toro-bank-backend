984f4b567dff1db129a3388912a27655805c8f79
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-12 17:06:18 -0300

    code cleanup and some teste coverage

--- ToroBank/Application.UnitTests/UseCases/BuyOrderTest.cs
+++ ToroBank/Application.UnitTests/UseCases/BuyOrderTest.cs
@@ -1,10 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ToroBank.Domain.Entities;
 
 namespace Application.UnitTests.UseCases
@@ -14,11 +11,9 @@ namespace Application.UnitTests.UseCases
         private List<Asset> mockAssetBase;
         private List<NegotiatedAssetItem> mockNegotiatedAssets;
         private List<MostNegotiatedAssetItem> mockMostNegotiatedAssetsFroLastSevenDays;
-        private List<UserAsset> userAssets;
         private UserAsset assetToSave;
         private decimal price, subtotal, balance;
         private User mockUser;
-        private PurchaseOrder purchaseOrder;
         private List<UserAsset> AssetsPurchasedByUser;
 
         public class NegotiatedAssetItem
@@ -72,8 +67,6 @@ namespace Application.UnitTests.UseCases
 
             mockPurchaseOrder = new PurchaseOrder() { User = mockUser };
             mockPurchaseOrderWithAssets = new PurchaseOrder() { User = mockUser, AcquiredAssets = AssetsPurchasedByUser };
-
-
             mockNegotiatedAssets = new List<NegotiatedAssetItem>();
 
             mockAssetBase = new List<Asset>() {
@@ -84,7 +77,6 @@ namespace Application.UnitTests.UseCases
                 new Asset{ Id =5, Name = "TORO4", Value = 115.98M },
             };
 
-            //Random values
             Random rndQuantity = new Random(30);
             Random rndUser = new Random(7);
             Random rndAsset = new Random(5);
@@ -102,8 +94,7 @@ namespace Application.UnitTests.UseCases
                 }
             }
 
-            //validation: if one or more assets was not added in list, add a registry
-            for (int i = 1; i <= 5; i++)
+            for (int i = 1; i <= mockAssetBase.Count(); i++)
             {
                 if (mockNegotiatedAssets.Count(f => f.Asset.Id == i) == 0)
                 {
@@ -118,75 +109,36 @@ namespace Application.UnitTests.UseCases
                                                             .OrderByDescending(f => f.Quantity).ToList();
             
         }
-       
-
-        [Test]
-        public void Should_have_only_5_most_negotiated_assets_from_last_7_days()
-        {
-            Assert.IsNotNull(mockMostNegotiatedAssetsFroLastSevenDays);
-            Assert.IsTrue(mockMostNegotiatedAssetsFroLastSevenDays.Count() == 5);
-        }
-
-        private List<MostNegotiatedAssetItem> ReturnMostNegotiatedAssetsFromLastDays(int days)
-        {
-            var mostNegotiatedOrdered = (from x in mockNegotiatedAssets
-                                                            .Where(f => f.AcquiredAt >= DateTime.Now.AddDays(-days).Date && f.AcquiredAt <= DateTime.Now.Date)
-                                                            .GroupBy(f => f.Asset)
-                                         select new MostNegotiatedAssetItem { Asset = x.First().Asset, Quantity = x.Sum(f => f.Quantity) })
-                                                            .OrderByDescending(f => f.Quantity).ToList();
-            return mostNegotiatedOrdered;
-        }
-
-
         
         private void ExecuteUseCase(PurchaseOrder purchaseOrder, MostNegotiatedAssetItem selectedAsset, int quantity)
         {
-           // userAssets = new List<UserAsset>();
             assetToSave = null;
-
-            ////3. Calcular o preço da ação vezes a quantidade e guardar o preço
-            
             price = selectedAsset.Asset.Value;
             subtotal = quantity * price;
-
-            ////4. Verificar saldo disponível na conta do usuario
             balance = purchaseOrder.User.Balance;
 
-            ////5. Se o saldo for menor que R$122.31 - NÃO REALIZAR COMPRA
             if(subtotal <= 0)
             {
                 throw new InvalidOperationException("Ativo inválido!");
             }
-            if (balance < subtotal)
+            else if (balance < subtotal)
             {
-                //nao realiza compra
-                ////5.1. retornar mensagem 'saldo insuficiente' para o usuario
                 throw new InvalidOperationException("Saldo insuficiente!");
             }
             else
             {
-                ////6. Se o saldo for maior que R$122.31 - REALIZAR COMPRA
-                //pesquisar ativos
                 assetToSave = purchaseOrder.AcquiredAssets
                                             .OrderBy(f=> f.LastModified)
                                             .FirstOrDefault(f => f.UserId == purchaseOrder.User.Id && f.AssetId == selectedAsset.Asset.Id);
 
-                ////6.1. Registrar quantidades de ativos SANB11 ao cliente.
                 if (assetToSave == null)
                 {
-                    ////6.1.1. Se nao houver ativos, criar e adicionar valor.
                     purchaseOrder.AcquiredAssets.Add(new UserAsset(selectedAsset.Asset.Id, purchaseOrder.User.Id, quantity));
-                    
-                    //userAssets.Add(new UserAsset(selectedAsset.Asset.Id, purchaseOrder.User.Id, quantity));
-                    //assetToSave = userAssets.FirstOrDefault(f => f.UserId == purchaseOrder.User.Id && f.AssetId == selectedAsset.Asset.Id);
                 }
                 else
                 {
-                    ////6.1.2. Atualiza quantidade do ativo existente ao cliente.
                     assetToSave.Quantity += quantity;
                 }
-
-                ////6.2. Debitar o valor do saldo do cliente.
                 purchaseOrder.User.Balance -= subtotal;
             }
         }
@@ -202,18 +154,22 @@ namespace Application.UnitTests.UseCases
             var selectedAsset = mostNegotiated.FirstOrDefault(a => a.Asset.Name == "SANB11");
             int quantity = 3;
 
+            Assert.NotNull(mockPurchaseOrder);
+            Assert.NotNull(mockPurchaseOrder.User);
+            Assert.NotNull(mockNegotiatedAssets);
             Assert.DoesNotThrow(() => ExecuteUseCase(mockPurchaseOrder, selectedAsset, quantity));
             Assert.IsNotNull(mockUser);
             Assert.GreaterOrEqual(mockUser.Balance, 0);
         }
 
-        //deve passar se não houver erro. OK
-        //se houver erro, retornar um InvalidOperationException - OK
-        //deve passar se o calculo da compra estiver correto. - ok
-        //se o saldo for acima do minimo para compra, deve registrar novas quantidades de ativos do sanbb ao cliente.
-        //se o saldo for acima do minimo para compra, saldo deve ser impactado após a finalização da compra.- ok
-        //nao deve passar se o saldo do usuario for menor que o subtotal - ok
-        //nao deve passar se o ativo selecionado for invalido: ex: selecionei o SANB11, mas veio TORO
+
+        [Test]
+        public void Should_have_only_5_most_negotiated_assets_from_last_7_days()
+        {
+            Assert.IsNotNull(mockMostNegotiatedAssetsFroLastSevenDays);
+            Assert.IsTrue(mockMostNegotiatedAssetsFroLastSevenDays.Count() == 5);
+        }
+
         [Test]
         public void Should_pass_if_balance_is_updated_after_operation()
         {
@@ -247,7 +203,6 @@ namespace Application.UnitTests.UseCases
             int quantity = 3;
 
             Assert.IsTrue(mockPurchaseOrderWithAssets.AcquiredAssets.Count() == 4);
-
             Assert.DoesNotThrow(() => ExecuteUseCase(mockPurchaseOrderWithAssets, selectedAsset, quantity));
             Assert.NotNull(mockPurchaseOrderWithAssets);
             Assert.NotNull(mockPurchaseOrderWithAssets.AcquiredAssets);
@@ -262,11 +217,9 @@ namespace Application.UnitTests.UseCases
             var selectedAsset = mostNegotiated.FirstOrDefault(a => a.Asset.Name == "SANB11");
             int quantity = 3;
             selectedAsset.Asset.Value = 0;
-           
             Assert.AreEqual(0, selectedAsset.Asset.Value);
             Assert.Throws<InvalidOperationException>(() => ExecuteUseCase(mockPurchaseOrder, selectedAsset, quantity));
         }
-
 
         [Test]
         public void Should_have_error_if_balance_is_insuficient()
@@ -274,7 +227,6 @@ namespace Application.UnitTests.UseCases
             var mostNegotiated = mockMostNegotiatedAssetsFroLastSevenDays;
             var selectedAsset = mostNegotiated.FirstOrDefault(a => a.Asset.Name == "SANB11");
             int quantity = 4;
-            
             Assert.Throws<InvalidOperationException>(() => ExecuteUseCase(mockPurchaseOrder, selectedAsset, quantity));
         }
     }
