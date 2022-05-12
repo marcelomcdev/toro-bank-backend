902adf55a0c83c8dd64c709547ec2d9e1a837d6a
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-12 18:25:38 -0300

    Add AssetRepository

---
+++ ToroBank/ToroBank.Application/Features/Assets/IAssetRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroBank.Application.Common.Interfaces.Repositories;
+using ToroBank.Domain.Entities;
+
+namespace ToroBank.Application.Features.Assets
+{
+    public interface IAssetRepository : IGenericRepository<Asset, int>
+    {
+
+    }
+}
