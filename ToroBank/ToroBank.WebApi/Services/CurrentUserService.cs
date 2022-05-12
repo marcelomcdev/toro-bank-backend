f0af34fc1ca9315b3b84fb0921d2705da7e68322
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-11 21:26:55 -0300

    Adds a checker to get user logged and responsible for updates in db registries

---
+++ ToroBank/ToroBank.WebApi/Services/CurrentUserService.cs
@@ -0,0 +1,17 @@
+﻿using ToroBank.Application.Common.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace ToroBank.WebApi.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            UserId = httpContextAccessor.HttpContext?.User?.Claims?.FirstOrDefault(c => c.Type == "sub_id")?.Value
+                     ?? "anonymous";
+        }
+
+        public string UserId { get; }
+    }
+}
