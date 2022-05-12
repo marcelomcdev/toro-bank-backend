e835a7d1408272cf78af3175570f68653752270b
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-11 21:24:23 -0300

    Add transfer controller

---
+++ ToroBank/ToroBank.WebApi/Controllers/TransferController.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToroBank.Application.Features.Transfer.Commands.ReceiveTransfer;
+using ToroBank.Application.Features.Users;
+
+namespace ToroBank.WebApi.Controllers
+{
+    [Route("api/[controller]/spb")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IUserRepository _userRepository;
+
+        public TransferController(IUserRepository userRepository, IMediator mediator)
+        {
+            _userRepository = userRepository;
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Transfer event notification.
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
+        [HttpPost("events")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> PostTransferEvent([FromBody] ReceiveTransferCommand cmd)
+        {
+
+            var response = await _mediator.Send(cmd);
+            return Created($"/api/[controller]/{response.Data}", null);
+        }
+    }
+}
