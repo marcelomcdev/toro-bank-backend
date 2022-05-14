d9f990f9526d1c4d99d729dc920817e20a9bb097
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-14 17:55:41 -0300

    Apply MainController to user and asset controllers

--- ToroBank/ToroBank.WebApi/Controllers/AssetController.cs
+++ ToroBank/ToroBank.WebApi/Controllers/AssetController.cs
@@ -1,18 +1,17 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using ToroBank.Application.Features.Users;
-using ToroBank.Application.Features.Users.Queries.GetUserById;
+using ToroBank.Application.Features.Assets;
+using ToroBank.Application.Features.Assets.Queries.GetMostTradedAssets;
 using ToroBank.WebApi.Helpers;
 
 namespace ToroBank.WebApi.Controllers
 {
-    //[ApiController]
-    public class UserController : MainController
+    public class AssetController : MainController
     {
         private readonly IMediator _mediator;
-        private readonly IUserRepository _userRepository;
+        private readonly IAssetRepository _userRepository;
 
-        public UserController(IUserRepository userRepository, IMediator mediator)
+        public AssetController(IAssetRepository userRepository, IMediator mediator)
         {
             _userRepository = userRepository;
             _mediator = mediator;
@@ -21,7 +20,7 @@ namespace ToroBank.WebApi.Controllers
         [HttpPost()]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Post([FromBody] GetUserByIdQuery cmd)
+        public async Task<IActionResult> Post([FromBody] GetMostTradedAssetsQuery cmd)
         {
             var response = await _mediator.Send(cmd);
             return Ok(response.Data);
