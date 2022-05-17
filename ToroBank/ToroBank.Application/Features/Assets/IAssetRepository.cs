0602d0642790855ca7b11bb3c2155e252344f449
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-17 20:07:31 -0300

    Add asset repository

--- ToroBank/ToroBank.Application/Features/Assets/IAssetRepository.cs
+++ ToroBank/ToroBank.Application/Features/Assets/IAssetRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ToroBank.Application.Common.DTOs.Assets;
-using ToroBank.Application.Common.Interfaces.Repositories;
+﻿using ToroBank.Application.Common.Interfaces.Repositories;
 using ToroBank.Application.Common.Wrappers;
 using ToroBank.Domain.Entities;
 
@@ -13,5 +7,6 @@ namespace ToroBank.Application.Features.Assets
     public interface IAssetRepository : IGenericRepository<Asset, int>
     {
         Task<PagedResponse<NegotiatedAsset>> GetMostNegotiatedAsset(int pageNumber, int pageSize);
+        Task<Asset> GetAssetBySymbol(string symbol);
     }
 }
