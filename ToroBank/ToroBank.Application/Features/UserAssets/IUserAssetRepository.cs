da08b60bb0fd483c78e90a1fdd4be7269672e142
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-17 20:08:54 -0300

    Add features and repository in infra layer

---
+++ ToroBank/ToroBank.Application/Features/UserAssets/IUserAssetRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroBank.Application.Common.Interfaces.Repositories;
+using ToroBank.Application.Common.Wrappers;
+using ToroBank.Domain.Entities;
+
+namespace ToroBank.Application.Features.UserAssets
+{
+    
+    public interface IUserAssetRepository : IGenericRepository<UserAsset, int>
+    {
+        //Task<PagedResponse<UserAsset>> GetAssets(int pageNumber, int pageSize);
+        Task<List<UserAsset>> GetAssetsByUserAsync(int userId);
+    }
+
+}
---
+++ ToroBank/ToroBank.Infrastructure/Repositories/UserAssetRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToroBank.Application.Features.UserAssets;
+using ToroBank.Domain.Entities;
+using ToroBank.Infrastructure.Persistence.Context;
+
+namespace ToroBank.Infrastructure.Persistence.Repositories
+{
+    public class UserAssetRepository : GenericRepository<UserAsset, int>, IUserAssetRepository
+    {
+        private readonly DbSet<UserAsset> _userAssets;
+        public UserAssetRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _userAssets = dbContext.Set<UserAsset>();
+        }
+
+        public async Task<List<UserAsset>> GetAssetsByUserAsync(int userId)
+        {
+            return await _userAssets.Where(f => f.UserId == userId).ToListAsync();
+        }
+
+       
+    }
+
+}
