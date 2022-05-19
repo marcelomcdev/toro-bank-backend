f4dc777baf3330a9839a8bc18af7de6b9328b093
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-18 21:14:30 -0300

    Add field investments to attend a use case

--- ToroBank/BankToro.Domain/Entities/UserPosition.cs
+++ ToroBank/BankToro.Domain/Entities/UserPosition.cs
@@ -5,6 +5,7 @@
         public decimal CheckingAccountAmount { get; set; }
         public decimal Consolidated { get; set; }
         public List<Position> Positions { get; set; }
+        public decimal Investments { get; set; }
         public UserPosition()
         {
             Positions = new List<Position>();
