9c045f852848f61b6e555d10e08c4b11428c935f
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-21 19:40:53 -0300

    Add configuration to call auth use case

---
+++ ToroBank/BankToro.Domain/Common/Configuration/GeneralConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToroBank.Domain.Common.Configuration
+{
+    public class GeneralConfig
+    {
+        public string Secret { get; set; }
+        public int ExpirationHours { get; set; }
+        public string Issuer { get; set; }
+        public IEnumerable<string> ValidIn { get; set; }
+    }
+}
