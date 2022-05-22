783d0616961cef92a7345392368b80f22fb71b5c
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-21 23:52:54 -0300

    Add error for invalid fields to use in testes

---
+++ ToroBank/ToroBank.Application/Common/Exceptions/InvalidFieldException.cs
@@ -0,0 +1,26 @@
+﻿namespace ToroBank.Application.Common.Exceptions
+{
+    public class InvalidFieldExcetion : ApplicationException
+    {
+        public InvalidFieldExcetion()
+            : base()
+        {
+        }
+
+        public InvalidFieldExcetion(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidFieldExcetion(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public InvalidFieldExcetion(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was invalid.")
+        {
+        }
+    }
+
+}
