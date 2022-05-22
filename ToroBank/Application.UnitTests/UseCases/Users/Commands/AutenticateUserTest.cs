e14db8a33eaee0c3df8e740e6f116a7cda46ae40
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-21 21:15:07 -0300

    Add setup and first test

--- ToroBank/Application.UnitTests/UseCases/Users/Commands/AutenticateUserTest.cs
+++ ToroBank/Application.UnitTests/UseCases/Users/Commands/AutenticateUserTest.cs
@@ -11,6 +11,14 @@ using System.Threading;
 using MediatR;
 using ToroBank.Application.Common.Wrappers;
 using ToroBank.Application.Common.Interfaces.Repositories;
+using ToroBank.Domain.Common.Configuration;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Linq;
+using ToroBank.Application.Features.Authentication;
 
 namespace Application.UnitTests.UseCases.Users.Commands
 {
@@ -70,9 +78,124 @@ namespace Application.UnitTests.UseCases.Users.Commands
     }
 
 
+    public class Token
+    {
+        public string? AccessToken { get; set; }
+        public string? Client { get; set; }
+        public string? UID { get; set; }
+    }
 
-   
+    public class TokenRequest
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+
+    public class TokenResponse
+    {
+        public Token? token { get; set; }
+    }
+
+    public interface ITokenService
+    {
+        Task<bool> IsValid(TokenRequest request);
+        Task<TokenResponse?> Authenticate(TokenRequest request);
+        Task<Token?> GenerateJwtToken(string email);
+    }
+
+    public class TokenService : ITokenService
+    {
+        private readonly Token? _token;
+        private readonly GeneralConfig _generalConfig;
+        //private readonly HttpContext _httpContext;
+
+        private readonly IUserRepository _userRepository;
+
+        public TokenService(IUserRepository userRepository, IOptions<GeneralConfig> generalConfig)
+        {
+            _userRepository = userRepository;
+            _generalConfig = generalConfig.Value;
+        }
+
+        public async Task<TokenResponse?> Authenticate(TokenRequest request)
+        {
+            if (await IsValid(request))
+            {
+                Token jwtToken = await GenerateJwtToken(request.Email);
+                return new TokenResponse() { token = jwtToken };
+            }
+            else
+                return null;
+        }
+
+        public async Task<bool> IsValid(TokenRequest request)
+        {
+            var user = await _userRepository.GetByEmailAsync(request.Email);
+            if (user == null)
+                return false;
+            else
+            {
+                if (user.Username.Equals(request.Email) && user.Password.Equals(request.Password))
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        public async Task<Token> GenerateJwtToken(string email)
+        {
+            var user = await _userRepository.GetByEmailAsync(email);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_generalConfig.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = _generalConfig.Issuer,
+                Audience = _generalConfig.ValidIn.First(),
+                Expires = DateTime.UtcNow.AddHours(_generalConfig.ExpirationHours),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
 
 
+            return new Token()
+            {
+                AccessToken = token,
+                Client = user?.Name,
+                UID = $"{user.Id}"
+            };
+        }
+
+       
+
+
+    }
+
+
+    public class AuthUserCommand : IRequest<Result<Token?>>
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+
+
+    public class AuthUserCommandHandler : IRequestHandler<AuthUserCommand, Result<Token?>>
+    {
+        private readonly IAuthRepository _authRepository;
+        private readonly ITokenService _tokenService;
+        public AuthUserCommandHandler(IAuthRepository authRepository, ITokenService tokenService)
+        {
+            _authRepository = authRepository;
+            _tokenService = tokenService;
+
+        }
+
+        public async Task<Result<Token?>> Handle(AuthUserCommand request, CancellationToken cancellationToken)
+        {
+            TokenResponse? response = await _tokenService.Authenticate(new TokenRequest() { Email = request?.Email, Password = request?.Password });
+            return Result<Token>.Ok(response?.token);
+        }
+    }
 
 }
